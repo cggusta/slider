c1ec760f06d7748d3c53b9d72e78f90d7777f992
Christopher Gustavson <30939099+cggusta@users.noreply.github.com>
2017-08-23 10:44:54 -0700

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+(function() {
+
+    $(document).ready(function() {
+
+        setInterval(function() {
+            moveLeft();
+        }, 5000);
+
+        var slideCount = $('#slider ul li').length;
+        var slideWidth = $('#slider ul li').width();
+        var slideHeight = $('#slider ul li').height();
+        var sliderUlWidth = slideCount * slideWidth;
+
+        $('#slider').css({ width: slideWidth, height: slideHeight });
+        $('#slider ul').css({ width: sliderUlWidth, marginLeft: -slideWidth });
+        $('#slider ul li:last-child').prependTo('#slider ul');
+
+        function moveLeft() {
+            $('#slider ul').animate({
+                left: -slideWidth
+            }, 1000, function() {
+                $('#slider ul li:first-child').appendTo('#slider ul');
+                $('#slider ul').css('left', '');
+            });
+        };
+
+    });
+
+})();
